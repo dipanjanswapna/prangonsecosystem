6c8423199e838414a20fb977f3b17e06b94478b6
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-12-09 18:50:18 +0000

    Try fixing this error: `Runtime Error: Failed to parse private key: Erro

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -10,9 +10,7 @@ let adminApp: App;
 if (adminApps.length > 0) {
     adminApp = adminApps[0];
 } else {
-    const privateKey = (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n');
-
-    if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_CLIENT_EMAIL || !privateKey) {
+    if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PRIVATE_KEY) {
         throw new Error('Firebase admin environment variables are not set.');
     }
     
@@ -20,7 +18,7 @@ if (adminApps.length > 0) {
         credential: cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: privateKey,
+            privateKey: (process.env.FIREBASE_PRIVATE_KEY).replace(/\\n/g, '\n'),
         }),
         databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`,
     }, 'firebase-admin-app');
