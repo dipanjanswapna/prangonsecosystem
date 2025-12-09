f7fd3cb81be3d8a5583179f6f79b35cd673266a4
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-12-09 15:52:33 +0000

    carosol er middle er picture ti aro onek boro koro tate jodi side er pic

--- src/components/hero-carousel.tsx
+++ src/components/hero-carousel.tsx
@@ -71,7 +71,7 @@ export function HeroCarousel() {
               <CarouselItem
                 key={index}
                 className={cn(
-                  'pl-2 md:pl-4 basis-full md:basis-1/2 lg:basis-2/5 transition-transform duration-300 ease-in-out',
+                  'pl-2 md:pl-4 basis-full md:basis-1/2 lg:basis-3/5 transition-transform duration-300 ease-in-out',
                   isCenter ? 'scale-100' : 'scale-75 opacity-60'
                 )}
               >
