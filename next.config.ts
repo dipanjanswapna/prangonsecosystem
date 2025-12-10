6629a26a74e99b9206db9d7d61cf959391ba50a6
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-12-10 14:36:29 +0000

    Cover Image URL e https://i.postimg.cc/dtjybvc2/photo-2025-10-11-09-11-4

--- next.config.ts
+++ next.config.ts
@@ -39,6 +39,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.ibb.co',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
