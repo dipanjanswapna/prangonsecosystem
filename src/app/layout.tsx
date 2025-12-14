4483c7ea685531badee84037e2a6952636e5c6bf
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-12-14 09:05:29 +0000

    dui pasher deyal theke dui pash diye space barao

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
         >
           <FirebaseClientProviderWrapper>
               <AppHeader />
-              <main className="flex-1">
+              <main className="flex-1 container px-4 md:px-6 lg:px-8 py-8">
                 {children}
               </main>
               <AppFooter />
