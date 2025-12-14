3c7888dda098e8f78e81c2720c5463338c773bd5
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-12-14 09:29:43 +0000

    main nevigation bar orthat home page er nevigation bar dashboard e jukto

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -13,6 +13,7 @@ const DashboardLayout: FC<DashboardLayoutProps> = ({ children }) => {
   return (
     <DashboardProvider>
       <div className="relative flex min-h-screen w-full flex-col bg-muted/40">
+        <AppHeader />
         <main className="flex-1 flex flex-col gap-4 p-4 md:gap-8 md:p-8 overflow-auto pb-20">
           {children}
         </main>
